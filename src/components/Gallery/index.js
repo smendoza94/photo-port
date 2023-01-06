413b4cf78e92f8d91b63adc5c8f07dd4da542473
StevenM <stevenbmendoza@gmail.com>
2023-01-05 23:41:49 -0800

    comment about props

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 import photo from "../../assets/small/commercial/0.jpg";
 
+// using props from Nav to determine category, Nav is in a lifted state
+// to be accessible for Gallery components
+
 function Gallery(props) {
   const currentCategory = {
     name: "commercial",
