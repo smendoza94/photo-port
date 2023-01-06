25f1b42f0cce03abb1f7ed0bdb30f05031b1f02e
StevenM <stevenbmendoza@gmail.com>
2023-01-05 23:46:58 -0800

    raise categories up to App lvl, pass setter through Nav comp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import About from "./components/About";
@@ -10,9 +10,30 @@ import Gallery from "./components/Gallery";
 // function is a component or just a regular function.
 
 function App() {
+  // Lift state to App.js, pass the currentCategory and Setter through to Nav,
+  // and move categories up to the App level.
+  const [categories] = useState([
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
         <Gallery></Gallery>
         <About></About>
