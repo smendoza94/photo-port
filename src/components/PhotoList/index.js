13a78a8b1c5b194362f5f7ca0924fec720bd2dc3
StevenM <stevenbmendoza@gmail.com>
2023-01-06 18:27:31 -0800

    pass category props to gallery, photolist, and app prop from nav comp user select

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,16 +1,151 @@
-import React from "react";
-import photo from "../../assets/small/commercial/0.jpg";
+import React, { useState } from "react";
 
-function PhotoList() {
+const PhotoList = ({ category }) => {
+  const [photos] = useState([
+    {
+      name: "Grocery aisle",
+      category: "commercial",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Grocery booth",
+      category: "commercial",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Building exterior",
+      category: "commercial",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Restaurant table",
+      category: "commercial",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Cafe interior",
+      category: "commercial",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Cat green eyes",
+      category: "portraits",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Green parrot",
+      category: "portraits",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Yellow macaw",
+      category: "portraits",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Pug smile",
+      category: "portraits",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Pancakes",
+      category: "food",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Burrito",
+      category: "food",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Scallop pasta",
+      category: "food",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Burger",
+      category: "food",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Fruit bowl",
+      category: "food",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Green river",
+      category: "landscape",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Docks",
+      category: "landscape",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Panoramic village by sea",
+      category: "landscape",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Domestic landscape",
+      category: "landscape",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+    {
+      name: "Park bench",
+      category: "landscape",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+    },
+  ]);
+
+  // Go through each photo in the photos array, find every photo that matches the category
+  // that was selected by the user. If a photo matches the condition, return in an array
+  // and assign to currentPhotos.
+  const currentPhotos = photos.filter((photo) => photo.category === category);
+
   return (
     <div>
-      <img
-        src={photo}
-        alt="Commercial Example"
-        className="img-thumbnail mx-1"
-      />
+      <div className="flex-row">
+        {/* // Map the currentPhotos array to render each photo that matches the category selected 
+      // by the user. */}
+        {currentPhotos.map((image, i) => (
+          <img
+            // The default property is where the image has been saved. To render the
+            // image, the default property must be invoked.
+            src={require(`../../assets/small/${category}/${i}.jpg`).default}
+            // The alt attribute is used for accessibility user-assistance devices,
+            // such as screen readers, so the image's name was assigned.
+            alt={image.name}
+            className="img-thumbnail mx-1"
+            // The key attribute was also assigned the image's name. This attribute
+            // value must be a unique string. The absence of this unique key value
+            // will cause an error message.
+            key={image.name}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default PhotoList;
